95fdb7049e28a1b3691fea8145ed252a687351a5
Joonwoo KIM <joonwookim@joonwoos-macbook-pro.local>
2018-07-29 21:23:22 -0400

    Initial Commit

---
+++ UWPortalRedesign/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UWPortalRedesign
+//
+//  Created by Joonwoo KIM on 2018-07-29.
+//  Copyright © 2018 Joonwoo KIM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
